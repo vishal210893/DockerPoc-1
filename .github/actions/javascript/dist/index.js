0fdba3a4b21ea9f65f166812177c38aa43f6b5df
viskumar <viskumar@guidewire.com>
2025-05-26 23:48:00 +0530

    feat: update @vercel/ncc to version 0.38.3 and add custom JavaScript GitHub Action guide in README
    Signed-off-by: viskumar <viskumar@guidewire.com>

--- .github/actions/javascript/dist/index.js
+++ .github/actions/javascript/dist/index.js
@@ -1,4 +1,4 @@
-require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
+/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 4914:
@@ -27725,4 +27725,3 @@ run();
 module.exports = __webpack_exports__;
 /******/ })()
 ;
-//# sourceMappingURL=index.js.map
